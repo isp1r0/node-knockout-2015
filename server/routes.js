e3c61bb6037650cd8449bfc637694855dd27c05f
Fran Varney <fran@modulus.io>
2015-11-07 01:04:25 -0500

    Add route

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,9 @@
+const Auth = require('./handlers/api/auth')
 const User = require('./handlers/api/user')
 
 module.exports = [
+  { method: 'POST', path: '/api/auth', handler: Auth.validate },
+
   { method: 'POST', path: '/api/user', handler: User.create },
   { method: 'DELETE', path: '/api/user/{id}', handler: User.delete },
   { method: 'GET', path: '/api/user/{id}', handler: User.find }
