7e646525889ca1a92158ddf194d626fe8609b463
Fran Varney <fran@modulus.io>
2015-11-07 02:55:27 -0500

    Change variable names

--- server/routes.js
+++ server/routes.js
@@ -1,22 +1,22 @@
-const Auth = require('./handlers/api/auth')
-const Item = require('./handlers/api/item')
-const List = require('./handlers/api/list')
-const User = require('./handlers/api/user')
+const AuthApi = require('./handlers/api/auth')
+const ItemApi = require('./handlers/api/item')
+const ListApi = require('./handlers/api/list')
+const UserApi = require('./handlers/api/user')
 
 module.exports = [
-  { method: 'POST', path: '/api/auth', handler: Auth.validate },
+  { method: 'POST', path: '/api/auth', handler: AuthApi.validate },
 
-  { method: 'GET', path: '/api/list/{id}/items', handler: Item.index },
-  { method: 'POST', path: '/api/item', handler: Item.create },
-  { method: 'DELETE', path: '/api/item/{id}', handler: Item.delete },
-  { method: 'GET', path: '/api/item/{id}', handler: Item.find },
+  { method: 'GET', path: '/api/list/{id}/items', handler: ItemApi.index },
+  { method: 'POST', path: '/api/item', handler: ItemApi.create },
+  { method: 'DELETE', path: '/api/item/{id}', handler: ItemApi.delete },
+  { method: 'GET', path: '/api/item/{id}', handler: ItemApi.find },
 
-  { method: 'GET', path: '/api/user/{id}/lists', handler: List.index },
-  { method: 'POST', path: '/api/list', handler: List.create },
-  { method: 'DELETE', path: '/api/list/{id}', handler: List.delete },
-  { method: 'GET', path: '/api/list/{id}', handler: List.find },
+  { method: 'GET', path: '/api/user/{id}/lists', handler: ListApi.index },
+  { method: 'POST', path: '/api/list', handler: ListApi.create },
+  { method: 'DELETE', path: '/api/list/{id}', handler: ListApi.delete },
+  { method: 'GET', path: '/api/list/{id}', handler: ListApi.find },
 
-  { method: 'POST', path: '/api/user', handler: User.create },
-  { method: 'DELETE', path: '/api/user/{id}', handler: User.delete },
-  { method: 'GET', path: '/api/user/{id}', handler: User.find }
+  { method: 'POST', path: '/api/user', handler: UserApi.create },
+  { method: 'DELETE', path: '/api/user/{id}', handler: UserApi.delete },
+  { method: 'GET', path: '/api/user/{id}', handler: UserApi.find }
 ]
