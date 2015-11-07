9b0f74bfcd4d5c62ef513bd1fb9c138faf506863
Fran Varney <fran@modulus.io>
2015-11-07 02:08:36 -0500

    Add list routes

--- server/routes.js
+++ server/routes.js
@@ -1,8 +1,14 @@
 const Auth = require('./handlers/api/auth')
+const List = require('./handlers/api/list')
 const User = require('./handlers/api/user')
 
 module.exports = [
   { method: 'POST', path: '/api/auth', handler: Auth.validate },
+
+  { method: 'GET', path: '/api/user/{id}/lists', handler: List.index },
+  { method: 'POST', path: '/api/list', handler: List.create },
+  { method: 'DELETE', path: '/api/list/{id}', handler: List.delete },
+  { method: 'GET', path: '/api/list/{id}', handler: List.find },
 
   { method: 'POST', path: '/api/user', handler: User.create },
   { method: 'DELETE', path: '/api/user/{id}', handler: User.delete },
