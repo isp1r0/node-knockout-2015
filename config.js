fbd55d9a0815769acfef73ef935e207bd335c3aa
Fran Varney <fran@modulus.io>
2015-11-06 23:39:59 -0500

    Add mongo uri option

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
 module.exports = {
   host: process.env.HOST || 'localhost',
+  mongo: {
+    uri: process.env.MONGO_URL || 'mongodb://localhost:27017/NKO'
+  },
   port: process.env.PORT || 8000
 }
