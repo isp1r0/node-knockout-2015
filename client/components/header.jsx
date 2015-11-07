4fb831c154175cebca214f9dbf78323491a1ac5d
Fran Varney <fran@modulus.io>
2015-11-07 16:25:14 -0500

    Start inline styles

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,10 +1,31 @@
 const React = require('react')
 
+var headerStyles = {
+  backgroundColor: 'red',
+  display: 'inline-block',
+  fontFamily: 'Oswald, sans-serif',
+  fontSize: '42',
+  fontWeight: '700',
+}
+
+var h1Styles = {
+  margin: '0',
+  padding: '0',
+  textTransform: 'uppercase'
+}
+
+var aStyles = {
+  color: 'white',
+  textDecoration: 'none'
+}
+
 const Header = React.createClass({
   render: function () {
     return (
-      <header>
-        <h1><a href='/'>Revisit</a></h1>
+      <header style={headerStyles}>
+        <h1 style={h1Styles}>
+          <a style={aStyles} href='/'>Revisit</a>
+        </h1>
       </header>
     )
   }
--- client/layouts/base.jsx
+++ client/layouts/base.jsx
@@ -4,17 +4,30 @@ const Footer = require('../components/footer.jsx')
 const Header = require('../components/header.jsx')
 const Nav = require('../components/nav.jsx')
 
+var bodyStyles = {
+  backgroundColor: '#EFEFEF',
+  margin: '0',
+  padding: '0',
+  width: '100%'
+}
+
+var pageStyles = {
+  margin: '0 auto',
+  width: '80%'
+}
+
 const Base = React.createClass({
   render: function () {
     return (
       <html>
         <head>
           <title>{this.props.title}</title>
+          <link rel='stylesheet' type='text/css' href='/public/styles/main.css' />
         </head>
-        <body>
+        <body style={bodyStyles}>
           <Header />
           <Nav />
-          <div className='page'>
+          <div style={pageStyles}>
             {this.props.children}
           </div>
           <hr />
